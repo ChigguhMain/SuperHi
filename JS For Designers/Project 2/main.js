d488c507209307580bc633276f87ede7b4ad08ec
unknown <weirdchimspankovich@gmail.com>
2020-03-12 23:25:56 +1100

    Up to video 6/24

--- JS For Designers/Project 2/main.js
+++ JS For Designers/Project 2/main.js
@@ -1,1 +1,6 @@
-// LOLOL
+const menuBtn = document.querySelector(".toggle-nav");
+const mainArea = document.querySelector("main");
+
+menuBtn.addEventListener("click", function() {
+    mainArea.classList.toggle("main-toggle");
+})
