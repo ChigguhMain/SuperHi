1a59cc4e7e0d39b5884268ca395858b6ac7c63fe
Chigguh <chigguh@Chigguh-Work.local>
2020-03-13 09:09:19 +1100

    git

--- JS For Designers/Project 2/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".toggle-nav");
-const mainArea = document.querySelector("main");
-
-menuBtn.addEventListener("click", function() {
-    mainArea.classList.toggle("main-toggle");
-})
---
+++ JS For Designers/Project 2/menu.js
@@ -0,0 +1,11 @@
+const toggleNav = document.querySelector(".toggle-nav");
+const mainArea = document.querySelector("main");
+
+toggleNav.addEventListener("click", function() {
+    mainArea.classList.toggle("open");
+    if(mainArea.classList.contains("open")){
+        toggleNav.innerHTML = `<img src="/JS For Designers/Project 2/assets/close.svg"/>Close`;
+    } else {
+        toggleNav.innerHTML = `<img src="/JS For Designers/Project 2/assets/menu.svg"/>Menu`;
+    }
+})
