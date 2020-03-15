04f010c64fc67cdf832df7de19267291daabd08d
unknown <weirdchimspankovich@gmail.com>
2020-03-15 13:42:29 +1100

    Adding basic clicking func. Need to cycle through. 17/24

--- JS For Designers/Project 2/slides.js
+++ JS For Designers/Project 2/slides.js
@@ -1,1 +1,15 @@
-// On click, cycle through the images by increasing the z-index. 
+// Storing HTML elements into variables
+const imageContainer = document.querySelector(".slides");
+const images = document.querySelectorAll(".slides img"); 
+
+// On click, increase the z-index of the next image in the array
+let slideNumber = 0;
+let z = 1;
+
+imageContainer.addEventListener("click", function(){
+    slideNumber = slideNumber + 1;
+    z = z + 1;
+
+    images[slideNumber].style.zIndex = z;
+    
+})
