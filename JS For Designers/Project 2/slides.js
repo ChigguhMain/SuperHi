6f0769291c518c400dfcb5ee91bdb8cb7f61feba
unknown <weirdchimspankovich@gmail.com>
2020-03-15 16:07:43 +1100

    Primary functionality all done. Final touched upcoming. 22/24

--- JS For Designers/Project 2/slides.js
+++ JS For Designers/Project 2/slides.js
@@ -1,19 +1,42 @@
-// Storing HTML elements into variables
+// Setting up the variables
 const imageContainer = document.querySelector(".slides");
 const images = document.querySelectorAll(".slides img"); 
-
-// On click, increase the z-index of the next image in the array
 let slideNumber = 0;
 let z = 1;
 
+// On click, change the image
 imageContainer.addEventListener("click", function(){
     slideNumber = slideNumber + 1;
     z = z + 1;
     
+    // Start slides again once they finish
     if(slideNumber > images.length - 1){
         slideNumber = 0;
     }
 
+    // Remove all fade animations
+    images.forEach(image => {
+        image.style.animation = "";
+    });
+
+    // Update slide number and z-index
     images[slideNumber].style.zIndex = z;
-    images[slideNumber].style.animation = "fade 0.5s";
+    images[slideNumber].style.animation = "fade 0.3s";
 });
+
+// On mouseover, move the images to random positions
+imageContainer.addEventListener("mouseenter", function(){
+    images.forEach(function(image){
+        const randX = 100 * Math.random() -50;
+        const randY = 100 * Math.random() -50;
+
+        image.style.transform = `translate(${randX}px, ${randY}px)`;
+    });
+});
+
+// On mouseout, move all the images back to their orignal positions
+imageContainer.addEventListener("mouseleave", function(){
+    images.forEach(function(image){
+        image.style.transform = "translate(0px, 0px)";
+    });
+})
