0175dec64bd585a4b60faa32d788c8fe66c34829
Chinmay Kulkarni <weirdchimspankovich@gmail.com>
2020-03-18 20:12:09 +1100

    Updated Project 2 with correct path name (no spaces)

--- JS-For-Designers/Project-2/menu.js
+++ JS-For-Designers/Project-2/menu.js
@@ -4,8 +4,8 @@ const mainArea = document.querySelector("main");
 toggleNav.addEventListener("click", function() {
     mainArea.classList.toggle("open");
     if(mainArea.classList.contains("open")){
-        toggleNav.innerHTML = `<img src="/JS For Designers/Project 2/assets/close.svg"/>Close`;
+        toggleNav.innerHTML = `<img src="/JS-For-Designers/Project-2/assets/close.svg"/>Close`;
     } else {
-        toggleNav.innerHTML = `<img src="/JS For Designers/Project 2/assets/menu.svg"/>Menu`;
+        toggleNav.innerHTML = `<img src="/JS-For-Designers/Project-2/assets/menu.svg"/>Menu`;
     }
 })
