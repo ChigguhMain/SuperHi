bcc65e290d4dc404faedb752c6443866f778929a
unknown <weirdchimspankovich@gmail.com>
2020-05-10 10:54:49 +1000

    Up to adding the javascript

--- JS-For-Designers/Project-3/typeChecker.js
+++ JS-For-Designers/Project-3/typeChecker.js
@@ -1,15 +1,5 @@
 const sentenceTag = document.querySelector(`input[type="text"]`);
-const outputTag = document.querySelector("section.output");
-const originalText = document.querySelector(".output").innerHTML;
 
-
-sentenceTag.addEventListener("keyup", function(){
-    
-    //input value ? output is input : output is original text.
-    if(this.value){
-        outputTag.innerHTML = this.value;
-    } else {
-        outputTag.innerHTML = originalText;
-    }
-
-});
+sentenceTag.addEventListener(`click`, () => {
+    console.log("testing");
+})
