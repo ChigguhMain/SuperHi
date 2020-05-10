afff3972dda4698c766f989974da410b215b957e
unknown <weirdchimspankovich@gmail.com>
2020-05-10 14:21:06 +1000

    Added input functionality. Video 8/20

--- JS-For-Designers/Project-3/typeChecker.js
+++ JS-For-Designers/Project-3/typeChecker.js
@@ -1,5 +1,18 @@
-const sentenceTag = document.querySelector(`input[type="text"]`);
+const sentenceTag = document.querySelector("input[type='text']");
+const outputTag = document.querySelector("section.output");
+const ogText = outputTag.innerHTML;
 
-sentenceTag.addEventListener(`click`, () => {
-    console.log("testing");
-})
+console.log(ogText);
+
+
+// When I type in sentenceTag, update outputTag accordingly.
+// If there is no value, put in the original text. 
+
+sentenceTag.addEventListener("keyup", function() {
+  if (this.value) {
+    outputTag.innerHTML = this.value;
+  } else {
+    outputTag.innerHTML = ogText;
+  };
+});
+
