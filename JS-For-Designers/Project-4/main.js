5a4fc1216bcd334c2ab31999d5256392aff45f74
unknown <weirdchimspankovich@gmail.com>
2020-05-13 00:34:54 +1000

    Progress bar on scroll

--- JS-For-Designers/Project-4/main.js
+++ JS-For-Designers/Project-4/main.js
@@ -1,8 +1,17 @@
 const pixelTag = document.querySelector("div.pixels");
+const bodyTag = document.querySelector("body");
+const progressTag = document.querySelector("div.progress");
 
 // Update pixelTag according to how much we've scrolled. 
 document.addEventListener("scroll", function(){
     let pixels = window.pageYOffset;
-    
     pixelTag.innerHTML = pixels;
+    
+    // When we scroll the page, make a progress bar that tracks the distance. 
+    let bodyHeight = bodyTag.getBoundingClientRect().height;
+    let scrollDistance = bodyHeight - window.innerHeight;
+    let percentage = pixels / scrollDistance;
+    progressTag.style.width = `${100 * percentage}%`;
+
 })
+
